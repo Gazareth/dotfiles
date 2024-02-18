81976d533a67e05a4b43c17710b0359b3ebcf9b7
Gazareth <persnicketygareth@gmail.com>
2024-02-18 14:39:51 +0000

    refactor(nvim): migrate buf close logic to functions directory

--- nvim/nvChad/custom/commands.lua
+++ nvim/nvChad/custom/commands.lua
@@ -1,3 +1,4 @@
+local close_empty_buffers = require("custom.functions.buffers").close_empty_buffers
 
 -- Global command to set current directory to the nvim config dir
 vim.api.nvim_create_user_command("CdHome", function()
@@ -17,74 +18,9 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
   end,
 })
 
-local bufCloseCandidates = {}
-
-local isBufCloseCandidate = function(bi)
-  if not vim.api.nvim_buf_is_valid(bi) then return 0 end
-  local bn = vim.api.nvim_buf_get_name(bi)
-  local line_count = vim.api.nvim_buf_line_count(bi)
-  local bt = vim.bo[bi].filetype
-
-  if (#bt == 0 and line_count == 1) or (#bn == 0 and bt == "alpha") then
-    -- Candidate for closing.
-    local bw = vim.fn.win_findbuf(bi)
-    local bwe = vim.fn.empty(bw)
-    if bwe == 1 then
-      return 1
-    elseif #bw == 1 then
-      -- Buffer is not hidden... it is being shown on at least one window!
-      -- But fear not! If these windows are the only ones in their tabs, we can close!
-      for _,wid in ipairs(bw) do
-        local tp = vim.api.nvim_win_get_tabpage(wid)
-        local ws = vim.api.nvim_tabpage_list_wins(tp)
-        if #ws == 1 then
-          return 2
-        end
-      end
-    end
-  end
-  return 0
-end
-
-local buf_clean = function()
-  for _,bi in ipairs(bufCloseCandidates) do
-    local c_score = isBufCloseCandidate(bi)
-    if c_score > 0 then
-      if c_score == 1 then
-        vim.cmd("bd "..bi)
-      elseif c_score == 2 then
-        local bw = vim.fn.win_findbuf(bi)
-        if #bw == 1 then
-          vim.api.nvim_win_close(bw[1], false)
-        end
-      end
-    end
-  end
-end
-
-local setupCloseBufCandidates = function()
-  -- Get valid buffers
-  local valid_bufs = vim.tbl_filter(function(bi)
-    return vim.api.nvim_buf_is_valid(bi)
-      and vim.api.nvim_buf_get_option(bi, 'buflisted')
-  end, vim.api.nvim_list_bufs())
-
-  local candidates = {}
-  for _, bi in ipairs(valid_bufs) do
-    if isBufCloseCandidate(bi) > 0 then
-      table.insert(candidates, bi)
-    else
-    end
-  end
-  if #candidates > 0 then
-    bufCloseCandidates = candidates
-    vim.defer_fn(buf_clean, 2000)
-  end
-end
-
 -- Close empty buffers when left
 vim.api.nvim_create_autocmd({ "BufLeave" }, {
-  callback = setupCloseBufCandidates
+  callback = close_empty_buffers
 })
 
 -- Highlight yanked text for a brief period after yanking
@@ -102,7 +38,7 @@ local open_config_files = function(left_rel_path, right_rel_path)
   if vim.bo.filetype == "alpha" then
     open_fn = "e"
   end
-vim.print("opening! ".. open_fn.." "..left_full_path .. (right_rel_path and (" | vsp "..right_full_path) or ""))
+-- vim.print("opening! ".. open_fn.." "..left_full_path .. (right_rel_path and (" | vsp "..right_full_path) or ""))
 
   vim.cmd(open_fn.." "..left_full_path .. (right_rel_path and (" | vsp "..right_full_path) or ""))
 end
@@ -112,7 +48,7 @@ local config_commands = {
     "lua/custom/plugins/overrides/alpha.lua"
   },
   ["EditKeyMappings"] = {
-    "lua/core/mappings.lua", "lua/custom/mappings.lua"
+    "lua/core/mappings.lua", "lua/custom/mappings/init.lua"
   },
   ["EditInstalledPlugins"] = {
     "lua/plugins/init.lua", "lua/custom/plugins/init.lua"
