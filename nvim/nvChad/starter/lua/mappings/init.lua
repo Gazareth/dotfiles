64d49054f4c87497fb933713a4f3b627b54b1f12
Gazareth <persnicketygareth@gmail.com>
2024-08-19 17:47:37 +0100

    fix(nvim): NvChad 2.5 migration - fix disabled hotkeys

--- nvim/nvChad/starter/lua/mappings/init.lua
+++ nvim/nvChad/starter/lua/mappings/init.lua
@@ -3,10 +3,14 @@ local glob_require = require("functions.globRequire").glob_require
 
 local mappings = glob_require("mappings")
 
-for _, section in pairs(mappings) do
-  for mode, maps in pairs(section) do
+for category, category_maps in pairs(mappings) do
+  for mode, maps in pairs(category_maps) do
     for key, val in pairs(maps) do
-      vim.keymap.set(mode, key, val[1], { desc = val[2] })
+      if category ~= "disabled" then
+        vim.keymap.set(mode, key, val[1], { desc = category .. " " .. val[2] })
+      else
+        vim.keymap.del(mode, key)
+      end
     end
   end
 end
