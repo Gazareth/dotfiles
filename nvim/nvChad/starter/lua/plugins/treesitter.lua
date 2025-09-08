c7ae74e8b50a4830c8daca8f563d2630a34e6f6d
Gazareth <persnicketygareth@gmail.com>
2025-09-08 23:13:20 +0100

    refactor(nvim): Auto formatting of a few plugin files

--- nvim/nvChad/starter/lua/plugins/treesitter.lua
+++ nvim/nvChad/starter/lua/plugins/treesitter.lua
@@ -1,49 +1,49 @@
-local M = {
-    {
+local M = {{
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate" -- Optional: Builds parsers on install/update
-    },
-  {
+}, {
     "nvim-treesitter/nvim-treesitter-textobjects",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    dependencies = {"nvim-treesitter/nvim-treesitter"},
     event = "VeryLazy",
-    dependencies = { "nvim-treesitter/nvim-treesitter" }, -- Ensure nvim-treesitter is installed
     after = "nvim-treesitter", -- Install after nvim-treesitter
     config = function()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+            ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html"},
             sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
+            highlight = {
+                enable = true
+            },
+            indent = {
+                enable = true
+            },
             select = {
                 enable = true,
                 lookahead = true,
                 keymaps = {
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
-                ["as"] = "@statement.outer",
-                ["is"] = "@statement.inner",
-                ["al"] = "@loop.outer",
-                ["il"] = "@loop.inner",
-                ["ap"] = "@parameter.outer",
-                ["ip"] = "@parameter.inner",
-                },
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = "@class.inner",
+                    ["as"] = "@statement.outer",
+                    ["is"] = "@statement.inner",
+                    ["al"] = "@loop.outer",
+                    ["il"] = "@loop.inner",
+                    ["ap"] = "@parameter.outer",
+                    ["ip"] = "@parameter.inner"
+                }
             },
             swap = {
                 enable = true,
                 swap_next = {
-                ["<leader>sn"] = "@parameter.inner",
+                    ["<leader>sn"] = "@parameter.inner"
                 },
                 swap_previous = {
-                ["<leader>sp"] = "@parameter.inner",
-                },
-            },
+                    ["<leader>sp"] = "@parameter.inner"
+                }
+            }
         })
     end
-  },
-}
+}}
 
 return M
--- nvim/nvChad/starter/lua/plugins/ui.lua
+++ nvim/nvChad/starter/lua/plugins/ui.lua
@@ -1,71 +1,55 @@
 local overrides = require("configs.overrides")
 
 -- UI
-local M = {
-  {
-    "nvim-telescope/telescope-fzf-native.nvim",  -- More efficient telescope search
+local M = {{
+    "nvim-telescope/telescope-fzf-native.nvim", -- More efficient telescope search
     build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
     lazy = false,
     config = function()
-      require("telescope").load_extension "fzf"
-    end,
-  },
-
-  {
-    "nvim-telescope/telescope-ui-select.nvim",  -- Replace neovim's default select UI with telescope
-      -- dependencies = "nvim-telescope/telescope.nvim",
-      lazy = false,
-      config = function()
+        require("telescope").load_extension "fzf"
+    end
+}, {
+    "nvim-telescope/telescope-ui-select.nvim", -- Replace neovim's default select UI with telescope
+    -- dependencies = "nvim-telescope/telescope.nvim",
+    lazy = false,
+    config = function()
         require('telescope').load_extension('ui-select')
-      end
-  },
-
-  {
+    end
+}, {
     "folke/trouble.nvim",
-    cmd = { "Trouble", "TroubleToggle", "TroubleRefresh", "TroubleClose" },  -- View diagnostic errors in one place
+    cmd = {"Trouble", "TroubleToggle", "TroubleRefresh", "TroubleClose"}, -- View diagnostic errors in one place
     dependencies = "nvim-tree/nvim-web-devicons",
     config = function()
-      require("trouble").setup {
-        vim.keymap.set("n", "gR", function() require("trouble").next({skip_groups = true, jump = true}); end,
-          {silent = true, noremap = true}
-        ),
-      }
+        require("trouble").setup {vim.keymap.set("n", "gR", function()
+            require("trouble").next({
+                skip_groups = true,
+                jump = true
+            });
+        end, {
+            silent = true,
+            noremap = true
+        })}
     end
-  },
-
-  -- IDE
-  {
-    "AndrewRadev/undoquit.vim",  -- Restore last quit buffer
+}, -- IDE
+{
+    "AndrewRadev/undoquit.vim", -- Restore last quit buffer
     cmd = "Undoquit",
     config = function()
-      vim.g.undoquit_mapping = ""
-      vim.g.undoquit_tab_mapping = ""
+        vim.g.undoquit_mapping = ""
+        vim.g.undoquit_tab_mapping = ""
     end
-  },
-
-  {
-    "numToStr/BufOnly.nvim",  -- Close all OTHER buffers
+}, {
+    "numToStr/BufOnly.nvim", -- Close all OTHER buffers
     cmd = "BufOnly"
-  },
-
-  {
-    "tpope/vim-fugitive",  -- Git actions
-    cmd = {
-      "G", "Git", "Gdiffsplit", "Gvdiffsplit", "Gedit", "Gsplit",
-      "Gread", "Gwrite", "Ggrep", "Glgrep", "Gmove",
-      "Gdelete", "Gremove", "Gbrowse",
-    },
-  },
-
-  {
-    'goolord/alpha-nvim',  -- Startup dashboard
+}, {
+    "tpope/vim-fugitive", -- Git actions
+    cmd = {"G", "Git", "Gdiffsplit", "Gvdiffsplit", "Gedit", "Gsplit", "Gread", "Gwrite", "Ggrep", "Glgrep", "Gmove",
+           "Gdelete", "Gremove", "Gbrowse"}
+}, {
+    'goolord/alpha-nvim', -- Startup dashboard
     lazy = false,
     config = overrides.alpha
-  },
-
-  {
-    "mbbill/undotree",  -- Access different undo "timelines"
-  }
-}
+}, {"mbbill/undotree" -- Access different undo "timelines"
+}}
 
 return M
