4b0286168246dc1f2848f746320b75843ea0b1d9
Gazareth <persnicketygareth@gmail.com>
2026-03-21 12:14:42 +0000

    feat(nvim): Add namu

---
+++ nvim/nvChad/starter/lua/plugins/navigation.lua
@@ -0,0 +1,24 @@
+-- Navigation
+local M = {
+  {
+    "bassamsdata/namu.nvim",
+    opts = {
+      global = {},
+      namu_symbols = { -- Specific Module options
+        options = {},
+      },
+    },
+    -- === Suggested Keymaps: ===
+    vim.keymap.set("n", "<leader>ew", ":Namu symbols<cr>", {
+      desc = "Jump to LSP symbol",
+      silent = true,
+    }),
+    vim.keymap.set("n", "<leader>sw", ":Namu workspace<cr>", {
+      desc = "LSP Symbols - Workspace",
+      silent = true,
+    }),
+    event = "BufEnter",
+  },
+}
+
+return M
