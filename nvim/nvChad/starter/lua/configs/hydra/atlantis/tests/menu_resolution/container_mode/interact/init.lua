2071b933832fbd078178fcbd35c208d195dbba46
Gazareth <persnicketygareth@gmail.com>
2026-04-24 14:25:02 +0100

    test(nvim): Atlantis (Beta) - add tests for Interact col in container mode

--- nvim/nvChad/starter/lua/configs/hydra/atlantis/tests/menu_resolution/container_mode/interact/init.lua
+++ nvim/nvChad/starter/lua/configs/hydra/atlantis/tests/menu_resolution/container_mode/interact/init.lua
@@ -1,8 +1,9 @@
 local M = "configs.hydra.atlantis.tests.menu_resolution.container_mode.interact."
 
 return function()
-  describe("Interact", function()
+  describe("Interact -", function()
     require(M .. "column")()
     require(M .. "reopen")()
+    require(M .. "outline")()
   end)
 end
